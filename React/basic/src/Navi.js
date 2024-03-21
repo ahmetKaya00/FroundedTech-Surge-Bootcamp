1d93b73356b9f518d17aef1b604d984a99136c87
Ahmet Kaya <academyakm0@gmail.com>
2024-03-21 13:30:22 +0300

    Navi Güncel

--- React/basic/src/Navi.js
+++ React/basic/src/Navi.js
@@ -1,11 +1,56 @@
-import React, { Component } from 'react'
-
-export default class Navi extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Sepetteki Ürün: {this.props.cart.length}</h2>
-      </div>
-    )
-  }
+import React from "react";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from "reactstrap";
+export default class Navi extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">Northwind App</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ms-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/components">components</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>options: {this.props.cart.length}</DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>option1</DropdownItem>
+                                    <DropdownItem>option2</DropdownItem>
+                                    <DropdownItem>Reset</DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
 }
